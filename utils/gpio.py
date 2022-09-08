0d02229d4155af265313c6c178b13f3edfdda7fc
lexito-o <lexito.contact@gmail.com>
2022-09-08 10:43:47 +0100

    cleanup

--- utils/gpio.py
+++ utils/gpio.py
@@ -1,18 +1,21 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-
 def setup(config):
     global relay_pin, toggled_time
     relay_pin = int(config['RELAY_PIN'])
     toggled_time = int(config['TOGGLED_TIME'])
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(relay_pin, GPIO.OUT)
 
 def toggle_door():
     global relay_pin, toggled_time
-    GPIO.output(relay_pin, GPIO.HIGH)
-    sleep(toggled_time)
-    GPIO.output(relay_pin, GPIO.LOW)
+    try:
+        GPIO.output(relay_pin, GPIO.HIGH)
+        sleep(toggled_time)
+        GPIO.output(relay_pin, GPIO.LOW)
+    except:
+        GPIO.cleanup()
+        return False
     GPIO.cleanup()
     return True
