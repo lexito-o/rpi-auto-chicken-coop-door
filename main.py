7486c29913635fdaedc7fcd10bea441d57df7089
lexito-o <lexito.contact@gmail.com>
2022-09-08 10:13:16 +0100

    initial configs

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from dotenv import dotenv_values
+from flask import Flask
+
+app = Flask(__name__)
+config = dotenv_values(".env")
+
+@app.route('/')
+def index():
+    return 'test'
+
+if __name__ == '__main__':
+    app.run(debug=True, host=config['HOST'], port=config['PORT'])
